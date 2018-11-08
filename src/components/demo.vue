2202ab071ca18045b2ace0bc0246c0fe383a6ee2
oomusou <oomusou@gmail.com>
2018-11-08 10:05:02 +0800

    Default VeeValidate

---
+++ src/components/demo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <input v-validate="'required|email'" name="email" type="text">
+    </div>
+    <div>
+      <span>{{ errors.first('email') }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VeeValidate from 'vee-validate';
+
+Vue.use(VeeValidate);
+
+export default {
+  name: 'demo',
+};
+</script>
+
+<style scoped>
+
+</style>
