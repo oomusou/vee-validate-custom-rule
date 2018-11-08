07c3e44fb23d7a7a43b325f8363ed4c6736c180a
oomusou <oomusou@gmail.com>
2018-11-08 11:14:15 +0800

    Custom Validator

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>
-      <input v-validate="'required|email'" name="email" type="text">
+      <input v-validate="'required|myEmail'" name="email" type="text">
     </div>
     <div>
       <span>{{ errors.first('email') }}</span>
@@ -11,15 +11,29 @@
 
 <script>
 import Vue from 'vue';
-import VeeValidate from 'vee-validate';
+import VeeValidate, { Validator } from 'vee-validate';
+import zh_TW from 'vee-validate/dist/locale/zh_TW';
+
+/** Custom Validator */
+const getMessage = field => `${field} 格式不正確`;
+
+const validate = value => {
+  const regex = /^\w+\.*\w+@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
+  return regex.test(value);
+};
+
+const myValidator = {
+  getMessage,
+  validate,
+};
 
 Vue.use(VeeValidate);
+Validator.localize('zh_TW', zh_TW);
+
+/** Custom Rule */
+Validator.extend('myEmail', myValidator);
 
 export default {
   name: 'demo',
 };
 </script>
-
-<style scoped>
-
-</style>
